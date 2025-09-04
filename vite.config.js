d6553bdd4594068c2defaffcf77facfb34505de3
MarcosBR315 <marcospaulo.pontes05@gmail.com>
2025-09-03 21:06:06 -0300

    Configurações pt.2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portal-atricon/'  // nome do repositório no GitHub
+  base: '/Portal-Atricon/'  // nome do repositório no GitHub
 });
